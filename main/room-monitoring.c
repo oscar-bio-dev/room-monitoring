f6645ccfb8508be63ee7098f0fe8ad8cdf8e7875
oscar-bio-dev <oscar_bio_dev@outlook.com>
2026-09-01 10:17:02 -0600

    chore: update copyright header to oscar-bio-dev

--- main/room-monitoring.c
+++ main/room-monitoring.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Oscar
+ * Copyright (c) 2026 oscar-bio-dev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
